31e6347abe631e7824fe657754fae9b90e914e71
andylevalley <andylevalley@gmail.com>
2018-11-29 11:18:33 -0500

    Add files via upload

--- Main.m
+++ Main.m
@@ -2,10 +2,33 @@ yalmip('clear');
 clear all
 clc
 
-% Model data
-SampleTime = 60; % (secs)
-MeanMotion = 7.291e-5; % (rad/sec)
-[A,B] = BuildDiscreteHCWmatrices(SampleTime, MeanMotion);
+SampleTime = 30; % (secs)
+n = 7.291e-5; % (rad/sec)
+
+A = [0,     0,      0,      1,      0,      0;
+     0,     0,      0,      0,      1,      0;
+     0,     0,      0,      0,      0,      1;
+    3*n^2,  0,      0,      0,      2*n,    0;
+     0,     0,      0,      -2*n,   0,      0;
+     0,     0,      -n^2,   0,      0,      0];
+ 
+ B = [0 0 0;
+      0 0 0;
+      0 0 0;
+      1 0 0;
+      0 1 0;
+      0 0 1];
+ 
+C = [1, 1,  1,  1,  1,  1];
+ 
+sys = ss(A,B,C,0);
+ 
+d_sys = c2d(sys,60);
+
+A = d_sys.A;
+B = d_sys.B;
+
+%% 
 
 T = 2*60*60/SampleTime; % number of discrete time steps
 nx = 6; % number of states
@@ -14,16 +37,16 @@ nu = 3; % number of control inputs
 % Define decision variables
 u = sdpvar(nu,T-1); % state decision variables
 x = sdpvar(nx,T); % control decision variables
-h = binvar(T-1,2); % 5 integer decision variables
+h = binvar(T-1,3); % 5 integer decision variables
 
-Mp = 10;
-Ms = 5;
-xw = [5 -5;
-      5 -5;
-      0   0;
-      0   0;
-      0   0;
-      0   0];
+Mp = 20;
+Ms = 10;
+xw = [5 -5 5;
+      5 -5 5;
+      5 -5 -5;
+      0   0 0;
+      0   0 0;
+      0   0 0];
 
 constraints = [x(1,1) == 0
                x(2,1) == 0
@@ -39,25 +62,30 @@ constraints = [x(1,1) == 0
                x(6,T) == 0];
            
 
+constraints = [constraints, -Mp <= x(1:3,:) <= Mp];
+constraints = [constraints, -Ms <= x(4:6,:) <= Ms];
+
 for i = 1:T-1
     
-    constraints = [constraints, x(1:6,i+1) == A*x(1:6,i) + B*u(1:3,i)];
+    constraints = [constraints, x(1:6,i+1) == A*x(1:6,i) + B*u(4:6,i)];
              
     constraints = [constraints
                    implies(h(i,1),x(1:6,i) == xw(1:6,1))
                    implies(h(i,2),x(1:6,i) == xw(1:6,2))
-                   implies(h(i,1),h(i,2) == 0)
-                   implies(h(i,2),h(i,1) == 0)];
+                   implies(h(i,3),x(1:6,i) == xw(1:6,3))];
+
+    constraints = [constraints, sum(h(i,:)) <= 1];
                
                        
 end
 
-constraints = [constraints, sum(h(:,1)) >= 10];
-constraints = [constraints, sum(h(:,2)) >= 10];
+constraints = [constraints, sum(h(:,1)) >= 1];
+constraints = [constraints, sum(h(:,2)) >= 1];
+constraints = [constraints, sum(h(:,3)) >= 1];
 
-objective = sum(sum(abs(u),2));
+objective = sum(sum(u,2));
 
-options = sdpsettings('solver','cplex');
+options = sdpsettings('solver','intlinprog');
 optimize(constraints,objective,options)
 
 %% Plot solution
@@ -79,9 +107,4 @@ plot(1:T,sol(1,:),1:T,sol(2,:))
 
 figure(3)
 plot(2:T,control(1,:),2:T,control(2,:),2:T,control(3,:))
-
-
-
-
-
 
